28c2bef65892f0e351efbecd7226b3fcb1b9eb6c
dhaile <dawityhaile@gmail.com>
2016-05-24 22:01:19 -0700

    added streaming module

---
+++ hala.rb
@@ -0,0 +1,19 @@
+class Hala
+
+  def initialize (io=$stdin, chunk=64*1024)
+    @io, @chunk = io, chunk
+  end
+
+  def stream (&block)
+    loop do
+      start = Time.now
+      data = @io.read(@chunk)
+      return if data.nil?
+
+      time = (Time.now - start).to_f
+      block.call(data, time)
+    end
+  end
+
+
+end
