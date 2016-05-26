1b5b4b9a42983a281db1092857d49f526cc45e29
dhaile <dawityhaile@gmail.com>
2016-05-26 00:00:54 -0700

    tadded funv

--- process.rb
+++ process.rb
@@ -22,3 +22,27 @@ img = Magick::ImageList.new("vw.jpg")
 
   pic8 = img.scale(0.60)         # Reduce to 60%
   pic9 = img.scale(400, 300)     # Reduce to 400x300
+def show_info(fname)
+  img = Magick::Image::read(fname).first
+  fmt = img.format
+  w,h = img.columns, img.rows
+  dep = img.depth
+  nc = img.number_colors
+  nb = img.filesize
+  xr = img.x_resolution
+  yr = img.y_resolution
+  res = Magick::PixelsPerInchResolution ? "inch" : "cm"
+
+  puts <<-EOF.gsub(/^\s+/, '')
+  File:       #{fname}
+  Format:     #{fmt}
+  Dimensions: #{w}x#{h} pixels
+  Colors:     #{nc}
+  Image size: #{nb} bytes
+  Resolution: #{xr}/#{yr} pixels per #{res}
+  EOF
+  puts
+end
+
+show_info("smallpic.jpg")
+show_info("vw.jpg")
